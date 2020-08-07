2a640239d431caa3971358f431481c71def38f82
nagln <laxminagln@gmail.com>
2020-08-07 22:45:26 +0530

    update

---
+++ covid19/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import {
+  FormControl,
+  Select,
+  MenuItem
+} from '@material-ui/core';
+import './App.css';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+          setCountries(countries);
+      });
+    };
+    getCountriesData();
+  }, []);
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+  return (
+    <div className="App">
+      <div className="app_header">
+        <h1>Covid 19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select
+            variant="outlined"
+            onChange={onCountryChange}
+            value={country}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app_stats">
+      </div>
+    </div>
+  );
+}
+
+export default App;
